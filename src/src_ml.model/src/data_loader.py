771736f99b205e043f930ba92cc7b7bc74c7d68e
unknown <preetpan910@gmail.com>
2025-06-25 16:19:21 +0530

    Done

--- src/src_ml.model/src/data_loader.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def prepare_features(pm25, pm10, no2, so2):
-    """
-    Prepares features for AQI prediction.
-    """
-    return np.array([[pm25, pm10, no2, so2]])
-
--- src/src_ml.model/src/model.py
+++
@@ -1,6 +0,0 @@
-from sklearn.ensemble import RandomForestRegressor
-
-def train_model(X_train, y_train):
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
-    return model
